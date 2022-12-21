2b830fbc133d661a7d32e66f362faae845a7f0c2
kjiirugriilian <kjiirugriilian@gmail.com>
2022-12-22 00:44:48 +0300

    с2

--- filter/Filter.js
+++ filter/Filter.js
@@ -9,22 +9,69 @@ let Filter = React.createClass({
     })),
   },
 
+  getInitialState: function() {
+    return {
+      inputText: '',
+      checkboxChecked: false,
+      listItems: this.props.listItems,
+    };
+  },
+
+  buttonClicked: function() {
+    this.setState({inputText: '', checkboxChecked: false, listItems: this.props.listItems});
+  },
+
+  inputTextChanged: function(eo) {
+    this.setState({inputText: eo.target.value}, this.filterListItems);
+  },
+
+  checkboxClicked: function(eo) {
+    this.setState({checkboxChecked: eo.target.checked}, this.filterListItems);
+  },
+
+  filterListItems: function() {
+
+    const inputText = this.state.inputText;
+    const checkboxChecked = this.state.checkboxChecked;
+    let listItemsArr = [...this.props.listItems];
+
+    listItemsArr = inputText
+      ? listItemsArr.filter(v => v.value.includes(inputText))
+      : listItemsArr;
+
+    listItemsArr = checkboxChecked
+      ? listItemsArr.sort((a, b) => (a.value > b.value) ? 1 : -1)
+      : listItemsArr;
+
+    this.setState({listItems: listItemsArr});
+
+  },
+
   render: function() {
 
-    let listCode = this.props.listItems.map(i =>
-      React.DOM.li({key: i.code}, i.value)
+    const listCode = this.state.listItems.map(v =>
+      React.DOM.li({key: v.code}, v.value)
     );
 
     return React.DOM.section({className: 'Filter'},
       React.DOM.div(null,
-        React.DOM.input({type: 'text', placeholder: 'Введите что-нибудь...'}),
-        React.DOM.button(null, 'Сбросить'),
+        React.DOM.input({
+          type: 'text',
+          placeholder: 'Введите что-нибудь...',
+          value: this.state.inputText,
+          onChange: this.inputTextChanged,
+        }),
+        React.DOM.button({onClick: this.buttonClicked}, 'Сбросить'),
       ),
       React.DOM.label(null,
-        React.DOM.input({type: 'checkbox'}),
-        React.DOM.span(null, 'Сортировать по алфавиту'),
+        React.DOM.input({
+          type: 'checkbox',
+          checked: this.state.checkboxChecked,
+          onClick: this.checkboxClicked,
+        }),
+        React.DOM.span(null, 'Упорядочить по алфавиту'),
       ),
-      React.DOM.ul(null, listCode),
+      React.DOM.ul({lang: 'en'}, listCode),
     );
 
   },
